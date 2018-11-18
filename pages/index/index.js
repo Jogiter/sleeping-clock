d797a51483214582caaea04d1b82c2f55bd51d6b
Jogiter <jogiter.g@gmail.com>
2018-11-18 20:04:29 +0800

    1

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,7 @@
 //index.js
 //获取应用实例
 const app = getApp();
-const domain = 'http://localhost:3000'
+const domain = "http://localhost:3000";
 
 Page({
   data: {
@@ -9,11 +9,10 @@ Page({
     hasUserInfo: false,
     hasSetClock: false,
     canIUse: wx.canIUse("button.open-type.getUserInfo"),
-    orderinfo: {},
+    orderList: {},
     orderTxt: {},
-    count:{},
+    count: {}
   },
-
 
   // 事件处理函数
   // setClock: function() {
@@ -22,76 +21,79 @@ Page({
   //   });
   // },
 
-
-
   renew: function() {
     wx.navigateTo({
       url: "../settime/index"
     });
   },
 
-//拼凑成订单日期数据
+  //拼凑成订单日期数据
   setOrderText: function(userinfo) {
-    var _this=this;
-    console.log(userinfo.start_date)
-    console.log(userinfo.push_days)
+    var _this = this;
+    console.log(userinfo.start_date);
+    console.log(userinfo.push_days);
 
-    let start = new Date(userinfo.start_date).toLocaleDateString().split('/').join('.')
-    let end = new Date(new Date(userinfo.start_date).getTime() + 3600 * 1000 * 24 * userinfo.push_days).toLocaleDateString().split('/').join('.')
-    let txt = `${start}-${end} 每天 ${userinfo.push_time}`
-    _this.setData({
-      orderTxt: txt
-    })
-    console.log(_this.data.orderTxt);
+    let start = new Date(userinfo.start_date)
+      .toLocaleDateString()
+      .split("/")
+      .join(".");
+    let end = new Date(
+      new Date(userinfo.start_date).getTime() +
+        3600 * 1000 * 24 * userinfo.push_days
+    )
+      .toLocaleDateString()
+      .split("/")
+      .join(".");
+    let txt = `${start}-${end} 每天 ${userinfo.push_time}`;
+    console.log(txt);
+    return txt;
+    // _this.setData({
+    //   orderTxt: txt
+    // })
   },
 
-
   getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+    console.log(e);
+    app.globalData.userInfo = e.detail.userInfo;
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
-    })
+    });
     wx.setStorage({
-      key: 'userInfo',
+      key: "userInfo",
       data: e.detail.userInfo,
       success: function() {
         console.log(e.detail.userInfo);
       }
-    })
+    });
 
     //如果用户有userinfo和openid，立即执行注册register
     if (this.data.hasUserInfo) {
-
       if (app.globalData.openid) {
         app.register();
       }
       wx.navigateTo({
-          url: '../settime/index',
-        }
-
-      )
+        url: "../settime/index"
+      });
     }
   },
 
   onLoad: function() {
-    var _this=this;
+    var _this = this;
 
     //获取用户userInfo
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
-      })
+      });
       wx.setStorage({
-        key: 'userInfo',
+        key: "userInfo",
         data: app.globalData.userInfo,
         success: function() {
           console.log("已有用户信息" + app.globalData.userInfo);
         }
-      })
-
+      });
     } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
@@ -101,60 +103,46 @@ Page({
         this.setData({
           userInfo: res.userInfo,
           hasUserInfo: true
-        })
+        });
         wx.setStorage({
-          key: 'userInfo',
+          key: "userInfo",
           data: res.userInfo,
           success: function() {
             console.log("已有用户信息2" + res.userInfo);
-
-
           }
-        })
-      }
+        });
+      };
     } else {
       // 在没有 open-type=getUserInfo 版本的兼容处理
       wx.getUserInfo({
         success: function(res) {
-          app.globalData.userInfo = res.userInfo
+          app.globalData.userInfo = res.userInfo;
           this.setData({
             userInfo: res.userInfo,
             hasUserInfo: true
-          })
+          });
           wx.setStorage({
-            key: 'userInfo',
+            key: "userInfo",
             data: res.userInfo,
             success: function() {
               console.log("已有用户信息3" + res.userInfo);
             }
-          })
+          });
         }
-      })
+      });
     }
 
+    if (app.globalData.openid) {
+      this.getOrder();
+      console.log("openid1" + app.globalData.openid);
+    } else {
+      app.openidCallback = res => {
+        app.globalData.openid = res.data.data.openid;
+        console.log("openid2" + app.globalData.openid);
 
-
-if(app.globalData.openid){
-
-  this.getOrder();
-  console.log('openid1'+app.globalData.openid);
-
-}
-else{
-
-  app.openidCallback = res =>{
-    app.globalData.openid=res.data.data.openid;
-    console.log('openid2'+app.globalData.openid);
-
-    this.getOrder();
-
-  }
-
-}
-
-
-
-
+        this.getOrder();
+      };
+    }
 
     //如果用户有userinfo和openid，立即执行注册register
     // if (this.data.hasUserInfo) {
@@ -168,56 +156,31 @@ else{
     // }
 
     //如果用户有openid,则请求orderlist数据，并执行得出orderTxt
-
-
   },
 
-
-  getOrder:function(){
-
-    var _this=this;
+  getOrder: function() {
+    var _this = this;
 
     //获取到了orderlist的信息
 
     if (app.globalData.openid) {
       wx.request({
-        url: 'http://alarm-env.ap-northeast-1.elasticbeanstalk.com/user',
+        url: "http://alarm-env.ap-northeast-1.elasticbeanstalk.com/user",
         data: {
           id: app.globalData.openid
         },
         success: function(res) {
-
-          if (res.data.data.orderlist.length > 0) {
-            _this.setData({
-              orderinfo: res.data.data.orderlist[0],
-            })
-            console.log(res.data.data.orderlist);
-            console.log(_this.data.orderinfo);
-          }
-
-          //给count赋值
+          let list = res.data.data.orderlist.map(item => {
+            return _this.setOrderText(item);
+          });
 
           _this.setData({
-            count:res.data.data.count,
-          })
-          console.log('count'+_this.data.count);
-
-
-//把orderinfo写成前端展示格式
-          if (_this.data.orderinfo) {
-            var user_info=_this.data.orderinfo;
-            _this.setOrderText(user_info);
-            _this.setData({
-              hasSetClock: true,
-            })
-            console.log("是否有订单"+_this.data.hasSetClock);
-
-          }
-
-
-
+            orderList: list,
+            count: res.data.data.count,
+            hasSetClock: res.data.data.orderlist.length > 0
+          });
         }
-      })
+      });
     }
   }
 });
--- pages/sleep/index.js
+++ pages/sleep/index.js
@@ -7,23 +7,21 @@ Page({
     userInfo: {},
     hasMusic: false,
     msgid: {},
-    msgInfo: {
-
-      avatar: 'http://img.jogiter.cn/lm.jpg',
-      nickname: '鲁鲁修的位',
-      title: '标题',
-      desc: '当浪漫褪色、浓情渐淡，柴米油盐的温暖何尝不是最长情的告白？ \
-       有玫瑰，有表白。但爱情，要承诺，更需坚守 请珍惜那个来了之后就再也没走的人。 \
-       愿年迈蹒跚，阳光和你仍在。 \
-       今晚，想想能一直伴在你身边的人和事物，于是便是最幸福的了。好好睡觉吧小可爱 ​​​​',
-      image: 'http://img.jogiter.cn/bg.jpg'
-    },
+    avatar: "http://img.jogiter.cn/lm.jpg",
+    nickname: "鲁鲁修的位",
+    title: "标题",
+    desc:
+      "当浪漫褪色、浓情渐淡，柴米油盐的温暖何尝不是最长情的告白？ \
+     有玫瑰，有表白。但爱情，要承诺，更需坚守 请珍惜那个来了之后就再也没走的人。 \
+     愿年迈蹒跚，阳光和你仍在。 \
+     今晚，想想能一直伴在你身边的人和事物，于是便是最幸福的了。好好睡觉吧小可爱 ​​​​",
+    image: "http://img.jogiter.cn/bg.jpg",
 
     audio: {
-      poster: '',
-      name: '',
-      author: '',
-      src: '',
+      poster: "",
+      name: "",
+      author: "",
+      src: ""
     },
 
     hasUserInfo: false,
@@ -31,22 +29,16 @@ Page({
     canIUse: wx.canIUse("button.open-type.getUserInfo")
   },
 
-
-
-
-
-
   onLoad: function(option) {
-
     this.setData({
-      msgid: option.id,
-    })
+      msgid: option.id
+    });
 
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
-      })
+      });
     } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
@@ -54,8 +46,8 @@ Page({
         this.setData({
           userInfo: res.userInfo,
           hasUserInfo: true
-        })
-      }
+        });
+      };
     } else {
       // 在没有 open-type=getUserInfo 版本的兼容处理
       wx.getUserInfo({
@@ -66,48 +58,35 @@ Page({
             hasUserInfo: true
           });
         }
-      })
-    };
+      });
+    }
 
     //get页面所需要的参数
     wx.request({
-      url: 'http://alarm-env.ap-northeast-1.elasticbeanstalk.com/msg',
+      url: "http://alarm-env.ap-northeast-1.elasticbeanstalk.com/msg",
       data: {
-        id: option.id,
+        id: option.id || 1
       },
       success: res => {
+        let data = res.data.data;
         this.setData({
-
-
-          'msgInfo.avatar':res.data.avatar,
-          'msgInfo.nickname':res.data.nickname,
-          'msgInfo.title':res.data.title,
-          'msgInfo.desc':res.data.desc,
-          'msgInfo.image':res.data.image,
-        })
+          avatar: data.avatar,
+          nickname: data.nickname,
+          title: data.title,
+          desc: data.desc,
+          image: data.image
+        });
         console.log(this.data.msgid);
-
-      },
-
-    })
-
+      }
+    });
   },
-
-
-
 
   onShareAppMessage: function(e) {
-
     return {
-      title: '今天睡了么，让我来哄你睡吧~',
-      path: "/pages/sleep/index?id=" + this.data.id,
-
-    }
-
+      title: "今天睡了么，让我来哄你睡吧~",
+      path: "/pages/sleep/index?id=" + this.data.id
+    };
   },
-
-
-
 
   getUserInfo: function(e) {
     console.log(e);
@@ -117,6 +96,4 @@ Page({
       hasUserInfo: true
     });
   }
-
-
-})
+});
