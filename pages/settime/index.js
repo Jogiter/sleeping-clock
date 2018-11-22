40af2c0e5ba4e62ca1797b206d0ca16e0403d0a6
unknown <772757422@qq.com>
2018-11-22 22:31:33 +0800

    1122

--- pages/settime/index.js
+++ pages/settime/index.js
@@ -4,6 +4,7 @@ const app = getApp();
 
 Page({
   data: {
+
     time: '请选择时间',
     date: '请选择日期',
     hasTime: false,
@@ -13,7 +14,8 @@ Page({
     hasUserInfo: false,
     canIUse: wx.canIUse("button.open-type.getUserInfo"),
     thirtysrc: 'https://img.jogiter.cn/clock/325119a429f02d3841137b739b373434ff02decb',
-    sevensrc: "https://img.jogiter.cn/clock/430da88d37c5b18fdb88a9643248d66e0cb2260a"
+    sevensrc: "https://img.jogiter.cn/clock/633052c5bc9da4342045deb32c98c2934c9f6f6f",
+
   },
 
   //时间处理函数
@@ -35,6 +37,7 @@ Page({
 
 
   onLoad: function() {
+    console.log(this.arrayData.tempid.length);
     var dd = new Date();
     dd.setDate(dd.getDate() + 1);
     var y = dd.getFullYear();
@@ -55,21 +58,7 @@ Page({
   push: function(event) {
     //判断日期和时间是否都选择了
     console.log(event.detail);
-    if (!this.data.hasDate) {
-
-      wx.showModal({
-        title: '请选择开始日期',
-        content: '请选择闹钟的开始日期',
-        showCancel: false,
-        confirmText: '好的',
-      });
-
-
-
-
-
-
-    } else if (!this.data.hasTime) {
+    if (!this.data.hasTime) {
       wx.showModal({
         title: '请选择闹钟时间',
         content: '请选择闹钟的推送提醒时间',
@@ -84,30 +73,43 @@ Page({
       //提交订单到后台
       console.log(event.detail.target.dataset.day);
       console.log(event.detail.formId);
-      wx.request({
-        url: "https://serverssl.szdazizai.com/setclock",
-        method: "POST",
-        data: {
-          uid: app.globalData.openid,
-          start_date: this.data.date,
-          push_days: event.detail.target.dataset.day,
-          push_time: this.data.time,
-          template_id: event.detail.formId,
-
-        },
-
-        success: function() {
-          //回到首页
-          wx.redirectTo({
-            url: "../index/index",
-          })
-          console.log('下单完成');
-        }
-      })
 
 
 
 
+
+      this.arrayData.tempid.push(event.detail.formId);
+
+
+      console.log('数组长度'+this.arrayData.tempid.length);
+      console.log(this.arrayData.tempid);
+
+    }
+
+
+    if(this.arrayData.tempid.length>=6){
+
+      //请求post
+        wx.request({
+          url: "https://serverssl.szdazizai.com/setclock",
+          method: "POST",
+          data: {
+            uid: app.globalData.openid,
+            start_date: this.data.start_date,
+            push_days: 6,
+            push_time: this.data.time,
+            template_id: this.arrayData.tempid,
+
+          },
+
+          success: function() {
+            //回到首页
+            wx.redirectTo({
+              url: "../index/index",
+            })
+            console.log('下单完成');
+          }
+        })
 
 
     }
@@ -120,6 +122,16 @@ Page({
 
 
   },
+
+
+
+  arrayData:{
+    tempid:[],
+
+  }
+
+
+
 
 })
 
